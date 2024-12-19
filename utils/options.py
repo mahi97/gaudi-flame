56189f132d53db47e02963f722ac2d79dbaa5b52
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2024-12-19 15:24:32 +0900

    added gaudi and eager boolean configs

--- utils/options.py
+++ utils/options.py
@@ -38,6 +38,7 @@ def args_parser():
     parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
     parser.add_argument('--all_clients', action='store_true', help='aggregation over all clients')
     parser.add_argument('--gaudi', type=bool, default=False, help='use gaudi instead of cuda')
+    parser.add_argument('--eager', type=bool, default=False, help='use eager mode for gaudi')
     _args = parser.parse_args()
     return _args
 
