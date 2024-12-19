027fc04f99ff2853d5e5ea6f44604247d8d57fa4
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2024-12-19 15:59:10 +0900

    fix args for docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,7 @@
 
 # Check if the required argument (image name) is provided
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 <image_name> [source_folder] [dataset_folder] [script_to_run]"
+  echo "Usage: $0 <image_name> [source_folder] [dataset_folder] [script_to_run] [script_args...]"
   exit 1
 fi
 
@@ -10,10 +10,13 @@ ROOT_NAME="workspace"
 # Get the image name from arguments
 IMAGE_NAME="$1"
 
-# Optional parameters for script, source folder, and dataset folder
+# Optional parameters for source folder, dataset folder, and script name
 SRC_FOLDER="${2:-}"
 DATASET_FOLDER="${3:-}"
 SCRIPT_NAME="${4:-}"
+
+# Collect additional arguments for the script
+SCRIPT_ARGS="${@:5}"
 
 # Function to extract the last directory name from a given path
 get_last_dir_name() {
@@ -36,7 +39,6 @@ else
   DATASET_MOUNT=""
 fi
 
-
 # Check if the image name is "base" and run vault.habana.ai/gaudi-docker/1.17.0/ubuntu22.04/habanalabs/pytorch-installer-2.3.1:latest
 if [ "$IMAGE_NAME" == "base" ]; then
   IMAGE_NAME="vault.habana.ai/gaudi-docker/1.17.0/ubuntu22.04/habanalabs/pytorch-installer-2.3.1:latest"
@@ -53,13 +55,13 @@ if [ -z "$SCRIPT_NAME" ]; then
   $SRC_MOUNT $DATASET_MOUNT \
   $IMAGE_NAME
 else
-  # Run the Docker container with the specified script
-  echo "Running the Docker container with script '$SCRIPT_NAME'..."
+  # Run the Docker container with the specified script and additional arguments
+  echo "Running the Docker container with script '$SCRIPT_NAME' and arguments '$SCRIPT_ARGS'..."
   sudo docker run -it --runtime=habana \
   -e HABANA_VISIBLE_DEVICES=all \
   -e OMPI_MCA_btl_vader_single_copy_mechanism=none \
   --cap-add=sys_nice --net=host --ipc=host \
   $SRC_MOUNT $DATASET_MOUNT \
   $IMAGE_NAME \
-  /$ROOT_NAME/$SRC_DIR_NAME/$SCRIPT_NAME
+  /$ROOT_NAME/$SRC_DIR_NAME/$SCRIPT_NAME $SCRIPT_ARGS
 fi
