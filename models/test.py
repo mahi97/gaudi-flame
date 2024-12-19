d53623a233a040269485c0c6a81d854bf69cf04b
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2024-12-19 15:31:24 +0900

    fix .cuda with .to(device) for test and add the support for lazy mode

--- models/test.py
+++ models/test.py
@@ -16,11 +16,15 @@ def test_img(net_g, datatest, args):
     data_loader = DataLoader(datatest, batch_size=args.bs)
     l = len(data_loader)
     for idx, (data, target) in enumerate(data_loader):
-        if args.gpu != -1:
-            data, target = data.cuda(), target.cuda()
+        # if args.gpu != -1:
+        data, target = data.to(args.device), target.to(args.device)
         log_probs = net_g(data)
         # sum up batch loss
-        test_loss += F.cross_entropy(log_probs, target, reduction='sum').item()
+        test_loss_0 = F.cross_entropy(log_probs, target, reduction='sum')
+        if args.gaudi and not args.eager:
+            htcore.mark_step()
+
+        test_loss += test_loss_0.item()
         # get the index of the max log-probability
         y_pred = log_probs.data.max(1, keepdim=True)[1]
         correct += y_pred.eq(target.data.view_as(y_pred)).long().cpu().sum()
